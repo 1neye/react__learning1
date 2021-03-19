ac965b5f5d6cfc589da730049744c9a9a7fd08fe
1neye <illya.landar@gmail.com>
2021-03-19 23:44:48 +0200

    codewars practicing

--- src/components/LearnJs/LearnJs.jsx
+++ src/components/LearnJs/LearnJs.jsx
@@ -41,13 +41,12 @@ let LearnJs = () => {
         //     alert(-getNum)
         // }
     }
-    let equalArr = [4, 4, 4, 4, 5, 4, 4 ,4]
+    let equalArr = [4, 4, 4, 4, 5, 4, 4, 4]
     let equalNumbersFunc = () => {
         if (equalArr[0] == equalArr[1]) {
             equalArr.shift()
             equalArr.shift()
-        }
-        else {
+        } else {
             return console.log(equalArr[0])
         }
 
@@ -56,9 +55,9 @@ let LearnJs = () => {
         num = prompt('Num?')
         let count = 1;
         let result = 0;
-        while(num >= count) {
+        while (num >= count) {
             result += count
-            count ++
+            count++
         }
         alert(result)
     }
@@ -86,6 +85,32 @@ let LearnJs = () => {
         console.log(result)
 
     }
+    let forInFunc = () => {
+        let arr = [1, 2, 3, 4, 5, 6, 7]
+        for (let key in arr) {
+            console.log(arr[key])
+        }
+    }
+    let forEachFunc = () => {
+        let arr = [1, 2]
+        arr.forEach(alert)
+    }
+
+    let oddOrEvenArrFunc = () => {
+        let arr = [1, 2, ]
+        let result = 0;
+        for(let i = 0; i < arr.length; i++){
+            result += arr[i];
+        }
+
+        if (result % 2 == 0) {
+            alert('even')
+        }
+        else {
+            alert('odd')
+        }
+    console.log(result)
+}
     return (
         <div>
             <span onClick={() => alert(`Privet ${name}`)}> Обратные кавычки ``</span>
@@ -94,9 +119,14 @@ let LearnJs = () => {
             <div onClick={() => whileFunc()}>While loop</div>
             <div onClick={() => changeNumFunc()}>Change number</div>
             <div onClick={() => equalNumbersFunc()}>Find same numbers in one array</div>
-            <div onClick={() => numPlusFunc()}>Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.</div>
+            <div onClick={() => numPlusFunc()}>Write a program that finds the summation of every number from 1 to num.
+                The number will always be a positive integer greater than 0.
+            </div>
             <div onClick={() => shortWordsFunc()}>Shortest words</div>
             <div onClick={() => reverseWordsFunc()}>Reverse words</div>
+            <div onClick={() => forInFunc()}>for in for arr</div>
+            <div onClick={() => forEachFunc()}>forEch</div>
+            <div onClick={() => oddOrEvenArrFunc()}>Sum array. And output odd or even</div>
         </div>
     )
 }
